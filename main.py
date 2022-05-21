a7e30cec7d134186fd3e09fc2fcfab98c68b1f07
mmtmn <42742390+mmtmn@users.noreply.github.com>
2022-05-21 10:02:34 -0300

    finish mendelbrot

--- main.py
+++ main.py
@@ -20,15 +20,18 @@ def is_stable(c, num_iterations):
         z = z ** 2 + c
     return abs(z) <= 2
 
-# np.warnings.filterwarnings("ignore")
+np.warnings.filterwarnings("ignore")
 
 def get_members(c, num_iterations):
     mask = is_stable(c, num_iterations)
     return c[mask]
 
-c = complex_matrix(-2, 0.5, -1.5, 1.5, pixel_density=512)
+c = complex_matrix(-2, 0.5, -1.5, 1.5, pixel_density=3000)
 members = get_members(c, num_iterations=20)
+
 plt.scatter(members.real, members.imag, color="black", marker=",", s=1)
+plt.imshow(is_stable(c, num_iterations=20), cmap="binary")
+
 plt.gca().set_aspect("equal")
 plt.axis("off")
 plt.tight_layout()
